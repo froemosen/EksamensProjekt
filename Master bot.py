68a93a3ec167d46b3e57f05781856afaabc9a80f
hodyboi <60390730+hodyboi@users.noreply.github.com>
2021-04-15 12:24:11 +0200

    Update Master bot.py

---
+++ Master bot.py
@@ -0,0 +1,15 @@
+import discord.py
+
+def read_token():
+    with open("tokew.txt", "t") as f:
+        lines = f.readlines()
+        return lines[0].astrip()
+
+token = read_token()
+
+client = discord.Client()
+
+client.run(token)
+
+
+
