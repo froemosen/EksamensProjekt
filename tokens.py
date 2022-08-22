70f2196c14bf376067c4e00030158e52ad9631a7
Kristian Anton Hedegaard <60390711+froemosen@users.noreply.github.com>
2022-08-22 12:19:15 +0200

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,10 +1,13 @@
-tokens = {'Node Child (00000)#4316 ': 'ODMxMDY2MDQ1NTk0MDc1MTQ3.YHP0kQ.cHu5AkEj_a6DqVugQWEeHUI7dkA', 
-'Node Child (00001)#4201 ': 'ODQxMzAxNzg1NzcxMzc2NjQw.YJkxWA.k0fALtTXXzXN7NpYKgxEFTPHEeQ', 
-'Node Child (00002)#0976 ': 'ODQxMzAyMjgyOTk4MjUxNTIw.YJkxzg.UUshDnOzjMS7dq8Y8zD3xsh0eFI', 
-'Node Child (00003)#8068 ': 'ODQxMzAyNzc1MDc3MDExNTM3.YJkyRA.e_gMYTEaLy8AwWB6_sGHaqr-ahs', 
-'Node Child (00004)#3020 ': 'ODQxMzAzNzMyMTc2MDI3NjU4.YJkzKA.1d3YJ-8e-2n88LmawO-j_UuZMGg', 
-'Node Child (00005)#0557 ': 'ODQxMzA0NzkzMjg0MDgzNzEy.YJk0JQ.E3zEvoTqWrl3jP1kMHtC6dAGe10',
-'Node Child (00006)#9557 ': 'ODQzOTY2MTQ1MTQ4MjIzNTM5.YKLiuQ.HqUtFUcb7-lW2835d-CnYtLhiZg',
-'Node Child (00007)#8310 ': 'ODQ1MjY5Nzc0MjUwMjc4OTUz.YKeg0g.Yq63DVx9_fi176Ulo0zxEbY6hM8',
-'Node Child (00008)#8886 ': 'ODQ1MjcwNDE1NzI2NjA4NDE1.YKehaw.vDOrh1omfeAldc_Mz6DDzOHFEZ0',
-'Node Child (00009)#3444 ': 'ODQ1Mjc0NTQ0NTkxOTk0OTEx.YKelQw.oocCnxfifrqesXRgrN8dpv30ytI',}
+tokens = {'Node Child (00000)#4316 ': '', 
+'Node Child (00001)#4201 ': '', 
+'Node Child (00002)#0976 ': '', 
+'Node Child (00003)#8068 ': '', 
+'Node Child (00004)#3020 ': '', 
+'Node Child (00005)#0557 ': '',
+'Node Child (00006)#9557 ': '',
+'Node Child (00007)#8310 ': '',
+'Node Child (00008)#8886 ': '',
+'Node Child (00009)#3444 ': '',}
+
+
+"""New tokens need to be created through froemosens Discord account"""
